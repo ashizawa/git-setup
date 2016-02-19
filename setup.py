cf7d30027b0aa7f2879210a596de6e41805d4881
ashizawa <ashizawa@rebelt.co.jp>
2016-02-19 18:35:53 +0900

    update script

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     install_requires=[],
     entry_points={
         'console_scripts':
-            'git-setup = git_setup:setup'
+            'git-setup = git_setup.main:setup'
     },
     zip_safe=False,
     classifiers=[
